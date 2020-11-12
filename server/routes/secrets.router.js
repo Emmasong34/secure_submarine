8c2d752aedc1355d05a37ea4c0e25d5e9eda330c
Emma Song Fisher <emmasongf@gmail.com>
2020-11-11 21:08:42 -0600

    base mode

---
+++ server/routes/secrets.router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+
+router.get('/', rejectUnauthenticated, (req, res) => {
+  
+  // res.send(req.user);
+  pool
+    .query(`SELECT ("content") FROM "secret" where "secret"."secrecy_level" < ${req.user.clearance_level};`)
+    .then((results) => res.send(results.rows))
+    .catch((error) => {
+      console.log('Error making SELECT for secrets:', error);
+      res.sendStatus(500);
+    });
+});
+
+module.exports = router;
